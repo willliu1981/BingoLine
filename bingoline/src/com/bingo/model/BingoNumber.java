a587f486188abe5ac3b1cf1b9f30f7b8f4f49d0a
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-02-16 00:53:06 +0800

    BingoNumber

--- bingoline/src/com/bingo/model/BingoNumber.java
+++ bingoline/src/com/bingo/model/BingoNumber.java
@@ -1,9 +1,81 @@
 package com.bingo.model;
 
+import java.awt.Point;
+
 public class BingoNumber {
 	private Integer number;
+	private Point point = new Point(-1, -1);
 	private Boolean check;
-	
-	
-	
+
+	public static void swap(BingoNumber b1, BingoNumber b2) {
+		Point temp = b1.point;
+		b1.point = b2.point;
+		b2.point = temp;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean res = false;
+		if (this == obj) {
+			res = true;
+		} else {
+			if (obj instanceof BingoNumber) {
+				BingoNumber other = (BingoNumber) obj;
+				if (this.number == null) {
+					if (other.number == null) {
+						res = true;
+					}
+				} else {
+					res = this.number.equals(other.number);
+				}
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.number.hashCode();
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+
+	public void setPoint(int x, int y) {
+		this.point.x = x;
+		this.point.y = y;
+	}
+
+	public Boolean getCheck() {
+		return check;
+	}
+
+	public void setCheck(Boolean check) {
+		this.check = check;
+	}
+
+	public void check() {
+		this.check = true;
+	}
+
+	public void uncheck() {
+		this.check = false;
+	}
+
+	public String toString() {
+		return String.format("[%d (%d,%d) %s]", this.number, this.point.x, this.point.y, this.check);
+	}
 }
--- bingoline/src/com/test/test/Test5.java
+++ bingoline/src/com/test/test/Test5.java
@@ -3,9 +3,12 @@ package com.test.test;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.bingo.model.BingoNumber;
+
 public class Test5 {
 
 	public static void main(String[] args) {
+		/*
 		Map<Car, Boolean> map = new HashMap<>();
 		Car c1 = new Car("c1", 1000);
 		Car c2 = new Car("c2", 2000);
@@ -23,7 +26,40 @@ public class Test5 {
 		c2.price=6666;
 		map.put(c2, false);
 		System.out.println(map);
-
+		
+		Object cc=new Car(null, 0);
+		cc=(Car)cc;
+		
+		Car c=(Car)cc;
+		*///
+		
+		Map<BingoNumber,Integer> map=new HashMap<>();
+		BingoNumber b1=new BingoNumber();
+		BingoNumber b2=new BingoNumber();
+		BingoNumber b3=new BingoNumber();
+		b1.setNumber(1);
+		b1.setPoint(1,1);
+		b2.setNumber(2);
+		b2.setPoint(2,2);
+		b3.setNumber(3);
+		b3.setPoint(3,3);
+		
+		map.put(b1, 10);
+		map.put(b2, 20);
+		map.put(b3, 30);
+		System.out.println(map);
+		
+		BingoNumber b4=new BingoNumber();
+		b4.setNumber(1);
+		b4.setPoint(4,4);
+		map.put(b4, 40);
+		System.out.println(map);
+		
+		b1.setPoint(5,5);
+		map.put(b1, 50);
+		System.out.println(map);
+		
+		
 	}
 
 	static class Car {
@@ -32,6 +68,22 @@ public class Test5 {
 
 		Car(String name, int price) {
 			this.name = name;
+			this.price = price;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Integer getPrice() {
+			return price;
+		}
+
+		public void setPrice(Integer price) {
 			this.price = price;
 		}
 
